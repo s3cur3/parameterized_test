4bd71a6a6de893b9bcd4d34f6b7c6c8693596de9
Tyler Young <tyler@tylerayoung.com>
2024-06-28 15:47:42 -0500

    Bump minimum required version

--- mix.exs
+++ mix.exs
@@ -7,7 +7,7 @@ defmodule ParameterizedTest.MixProject do
     [
       app: :parameterized_test,
       version: "0.1.0",
-      elixir: "~> 1.12",
+      elixir: "~> 1.14",
       elixirc_paths: elixirc_paths(Mix.env()),
       start_permanent: Mix.env() == :prod,
       aliases: aliases(),
